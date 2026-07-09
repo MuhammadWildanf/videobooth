d7b09f86eab2aa841273111a2bd5416a9d03b705
Muhammad Wildan Ferdiansyah <92373802+MuhammadWildanf@users.noreply.github.com>
2026-07-09 17:25:10 +0700

    fix: make PORT dynamic from env

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ const snapApi = new midtransClient.Snap({
 const paymentCache = {};
 // Initialize Express App
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 // Middleware
 app.use(cors());
