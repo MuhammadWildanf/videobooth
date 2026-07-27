b2a1558a8db3ef65d9d2b564e3a3c651d2519a6a
Muhammad Wildan Ferdiansyah <92373802+MuhammadWildanf@users.noreply.github.com>
2026-07-28 01:19:44 +0700

    Revert review UI to display both photo and video, hide camera frame

--- public/js/index.js
+++ public/js/index.js
@@ -250,7 +250,7 @@
                         const cdAreaPhoto = document.getElementById('countdown-area-photo');
                         if (cdAreaPhoto) cdAreaPhoto.innerText = 'Take a Photo';
                     }
-                } else if (state === 'review-video' || state === 'review-final') {
+                } else if (state === 'review-video') {
                     // PLAYBACK RECORDED VIDEO
                     if (webcamEl) webcamEl.classList.add('hidden');
                     if (previewEl) {
